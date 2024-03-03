efbe213a57a192dd5b59876a86c2eb68f93ad8f1
deepak mehra <deepak7mhera7@gmail.com>
2024-03-03 18:50:09 +0530

    last commit

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -4,7 +4,7 @@ export default function NavItem({bg,name}) {
     const arr = ["bg-[#0a0f28]","bg-[#333fd3]","bg-[#5a46af]"]
     const color = arr[0];
   return (
-    <div className={`${arr[bg]} bg-red-50  py-3 px-5 rounded-3xl`}>
+    <div className={`${arr[bg]}  py-3 px-5 rounded-3xl`}>
        <div>{name}</div>
       
     </div>
